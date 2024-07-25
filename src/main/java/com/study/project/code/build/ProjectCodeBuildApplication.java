2ddb18a1c05bcf5b5bdb25bf6a5c9f867c3cec2e
黄启军 <huangqijun@kcallme.com>
2024-07-25 11:00:49 +0800

    create

---
+++ src/main/java/com/study/project/code/build/ProjectCodeBuildApplication.java
@@ -0,0 +1,13 @@
+package com.study.project.code.build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCodeBuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCodeBuildApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/project/code/build/ProjectCodeBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.project.code.build;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCodeBuildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
